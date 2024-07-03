4d294a76645e0054fc43cfd3f14b64209f44197d
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-03 23:00:20 +0700

    Разметка для Navbar. Доп компонент для пункта меню. Для иконок корзины, аккаунта и пр. не стала делать отдельный компонент. Они все же разные

--- src/components/Common/Navbar/Navbar.jsx
+++ src/components/Common/Navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import NavbarMenuItem from '../NavbarMenuItem/NavbarMenuItem'
 
+const menuItems = [
+  'Clothes',
+  'Accessories',
+  'Body Type',
+  'Appearance'
+]
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className="navbar__top">
+        <p className="navbar__offer">Enjoy Complimentary Shipping on All Orders Worldwide!</p>
+      </div>
+      <div className="navbar__bottom">
+        <img className="navbar__log" src='' alt='logo'/>
+        <div className="navbar__menu">
+          {menuItems.map((item, index)=>{
+            return (
+              <NavbarMenuItem key={index} text={item}/>
+            )
+          })}
+        </div>
+        <div className="navbar__actionsWrap">
+            <img className="navbar__actionImg" src="" alt="search"/>
+            <img className="navbar__actionImg" src="" alt="account"/>
+            <img className="navbar__actionImg" src="" alt="cart"/>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Common/NavbarMenuItem.jsx/NavbarMenuItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NavbarMenuItem = ({text}) => {
+  return (
+    <div className="navbar__menuItem">
+      {text}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={0.5}
+        stroke="currentColor"
+        className="size-1"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m19.5 8.25-7.5 7.5-7.5-7.5"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default NavbarMenuItem;
