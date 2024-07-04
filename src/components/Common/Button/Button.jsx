096177283c8043574d02773ed046498c45d5973c
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-04 23:41:21 +0700

    Сделан компонент topcollection и стили к нему. Что-то надо делать со стилями базовыми. Надо как-то прикрепить их ко всему, чтобы не импортировать в каждый отдельный scss файл. Также стоит посмотреть еще варианты сделать то, что сделано в стилях topcollection - чтобы картинка была как фон (background img считаю, что не подходит, не хочу inlince css, а путь к файлу в последствие будет через запрос к сурверу), есть повторение свойств height - не нравится повтор

---
+++ src/components/Common/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Button.scss'
+
+const Button = ({children}) => {
+  return (
+    <div className='button'>
+      {children}
+    </div>
+  )
+}
+
+export default Button
