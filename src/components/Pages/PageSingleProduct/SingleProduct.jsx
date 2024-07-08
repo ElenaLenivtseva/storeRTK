dbee7719e899f35a8472f79ed98db0ac29decf3d
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-08 22:34:21 +0700

    Приблизительное строение папки для страницы единичного товара. Вероятно, разобью иначе: мельче

---
+++ src/components/Pages/PageSingleProduct/SingleProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../Common/Footer/Footer'
+import Navbar from '../../Common/Navbar/Navbar'
+
+const SingleProduct = () => {
+  return (
+    <div className='singleProduct'>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default SingleProduct
