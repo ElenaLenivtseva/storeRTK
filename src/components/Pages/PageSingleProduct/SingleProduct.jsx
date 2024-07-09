17523c317a081ab14b7f78bf04551d866683c602
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-09 23:20:48 +0700

    Сделана предварительная разметка для singleproductcard. Вероятно, вынесу option в отдельный компонент. Стилей пока нет. Пишу код с температурой, поэтому, вероятно, от комитов сегодняшнего дня начнутся проблемы

--- src/components/Pages/PageSingleProduct/SingleProduct.jsx
+++ src/components/Pages/PageSingleProduct/SingleProduct.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../../Common/Footer/Footer'
 import Navbar from '../../Common/Navbar/Navbar'
 import Recommendations from './Recommendations/Recommendations'
+import SingleProductCard from './SingleProductCard/SingleProductCard'
 
 const SingleProduct = () => {
   return (
     <div className='singleProduct'>
       <Navbar/>
+      <SingleProductCard/>
       <Recommendations/>
       <Footer/>
     </div>
---
+++ src/components/Pages/PageSingleProduct/SingleProductCard/SingleProductCard.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Button from "../../../Common/Button/Button";
+import LinkCamp from '../../../Common/LinkComp/LinkComp'
+import "./SingleProductCard.scss";
+
+const SingleProductCard = () => {
+  return (
+    <section className="singleProductCard">
+      <div className="wrap">
+        <div className="singleProductCard__slider"></div>
+        <div className="singleProductCard__info">
+          <div className="singleProductCard__top">
+            <div className="singleProductCard__header">
+              <h3 className="singleProductCard__subtitle"></h3>
+              <img className="singleProductCard__icon" src="" alt="" />
+            </div>
+            <div className="singleProductCard__rating">
+              <p className="singleProductCard__amountOfReviews">
+                [118 reviews]
+              </p>
+            </div>
+            <p className="singleProductCard__price">$420</p>
+          </div>
+          <div className="singleProductCard__bottom">
+            <div className="singleProductCard__option">
+              <div className="singleProductCard__optionTop">
+                <p className="singleProductCard__optionTitle">Color</p>
+                <p className="singleProductCard__optionSelected">[Noir]</p>
+              </div>
+              <div className="singleProductCard__optionWrap singleProductCard__colorsWrap">
+                <div className="singleProductCard__color"></div>
+              </div>
+            </div>
+            <div className="singleProductCard__option">
+              <div className="singleProductCard__optionTop">
+                <p className="singleProductCard__optionTitle">Size</p>
+                <p className="singleProductCard__optionSelected">[M]</p>
+              </div>
+              <div className="singleProductCard__optionWrap singleProductCard__sizeWrap">
+                <Button className="button_withoutOutline">S</Button>
+              </div>
+              <LinkCamp>What's my size?</LinkCamp>
+            </div>
+            <div className="singleProductCard__features">
+                <p className="singleProductCard__material">Cotton</p>
+                <p className="singleProductCard__bodyType">Apple Body Type</p>
+                <p className="singleProductCard__appearanceType">Dramatic</p>
+                <p className="singleProductCard__colorType">Cold Winter</p>
+            </div>
+            <div className="singleProductCard__amount">
+                +<input type="number"/>-
+            </div>
+            <p className="singleProductCard__note">Model|Ella is 1.78m tall and wears a size S</p>
+          </div>
+          <Button className="button_black">S</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SingleProductCard;
