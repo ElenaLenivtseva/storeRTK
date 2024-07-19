ff5f71a1d47735250b48531193223022adf375f2
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-19 23:40:12 +0700

    Решила сначала сверстать все напрямик в AccountContent. Потом уже буду все разделять, отрисовывать по пропсам и тп

--- src/components/Pages/AccountPage/AccountContent/AccountContent.jsx
+++ src/components/Pages/AccountPage/AccountContent/AccountContent.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
-import './AccountContent.scss'
+import React from "react";
+import Button from "../../../Common/Button/Button";
+import "./AccountContent.scss";
 
 const AccountContent = () => {
   return (
-    <div className='accountContent'>
-      AccountContent
+    <div className="accountContent">
+      <h3 className="accountContent__title">My Personal Information</h3>
+      <div className="accountContent__block">
+        <div className="accountContent__wrap">
+          <div className="accountContent__info">
+            <p className="accountContent__subtitle">First Name</p>
+            <p className="accountContent__text">Chloe</p>
+          </div>
+          <div className="accountContent__info">
+            <p className="accountContent__subtitle">Birthday</p>
+            <p className="accountContent__text">09/06/1995</p>
+          </div>
+          <div className="accountContent__info">
+            <p className="accountContent__subtitle">Phone Number</p>
+            <p className="accountContent__text">+1 212-531-3248</p>
+          </div>
+        </div>
+        <div className="accountContent__wrap">
+          <div className="accountContent__info">
+            <p className="accountContent__subtitle">Last Name</p>
+            <p className="accountContent__text">+1 212-531-3248</p>
+          </div>
+          <div className="accountContent__info">
+            <p className="accountContent__subtitle">Gender</p>
+            <p className="accountContent__text">Woman</p>
+          </div>
+        </div>
+        <div className="accountContent__wrap">
+          <Button className="accountContent__button">Edit</Button>
+        </div>
+      </div>
+      <div className="accountContent__block">
+        <div className="accountContent__wrap">
+          <p className="accountContent__subtitle">Email</p>
+          <p className="accountContent__text">chloenicker@gmail.ru</p>
+        </div>
+        <div className="accountContent__wrap">
+          
+        </div>
+        <div className="accountContent__wrap">
+          <Button className="accountContent__button">Edit</Button>
+        </div>
+      </div>
+      <div className="accountContent__block">
+        <div className="accountContent__wrap">
+          <p className="accountContent__subtitle">Password</p>
+          <p className="accountContent__text">******</p>
+        </div>
+        <div className="accountContent__wrap">
+          
+        </div>
+        <div className="accountContent__wrap">
+          <Button className="accountContent__button">Edit</Button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AccountContent
+export default AccountContent;
