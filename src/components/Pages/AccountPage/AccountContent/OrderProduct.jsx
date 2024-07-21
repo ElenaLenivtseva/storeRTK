50c32ffa85fb4d70d9632e9716ada55a63437a32
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-21 14:12:23 +0700

    Небольшое добавление к предыдущему коммиту: status у order мб и recived, и in progress. В связи с этим классы добавляются к соответсвующему элементу для стилей

--- src/components/Pages/AccountPage/AccountContent/OrderProduct.jsx
+++ src/components/Pages/AccountPage/AccountContent/OrderProduct.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
+import React from "react";
 import Button from "../../../Common/Button/Button";
 
-const OrderProduct = ({info}) => {
+const OrderProduct = ({ info }) => {
   return (
     <div className="accountContent__block">
-        <div className="accountContent__wrap accountContent__wrapImg">
-          <img
-            className="accountContent__img"
-            src={info.img}
-            alt={info.title}
-          />
+      <div className="accountContent__wrap accountContent__wrapImg">
+        <img className="accountContent__img" src={info.img} alt={info.title} />
+      </div>
+      <div className="accountContent__wrap">
+        <div className="accountContent__info accountContent__infoStretch">
+          <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
+            Order Date:
+          </p>
+          <p className="accountContent__text">{info.date}</p>
         </div>
-        <div className="accountContent__wrap">
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Order Date:
-            </p>
-            <p className="accountContent__text">{info.date}</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Status
-            </p>
-            <p className={`accountContent__status accountContent__status${info.status}`}>
-              {info.status}
-            </p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Amount
-            </p>
-            <p className="accountContent__text">{info.amount} {info.amount>1?'item':'items'}</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Cost
-            </p>
-            <p className="accountContent__text">${info.price*info.amount}</p>
-          </div>
+        <div className="accountContent__info accountContent__infoStretch">
+          <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
+            Status
+          </p>
+          <p
+            className={`accountContent__status accountContent__status${
+              info.status === "In Progress" ? "Progress" : "Recieved"
+            }`}
+          >
+            {info.status}
+          </p>
         </div>
-        <div className="accountContent__wrap">
-          <Button className="accountContent__button">View Product</Button>
+        <div className="accountContent__info accountContent__infoStretch">
+          <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
+            Amount
+          </p>
+          <p className="accountContent__text">
+            {info.amount} {info.amount > 1 ? "item" : "items"}
+          </p>
+        </div>
+        <div className="accountContent__info accountContent__infoStretch">
+          <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
+            Cost
+          </p>
+          <p className="accountContent__text">${info.price * info.amount}</p>
         </div>
       </div>
-  )
-}
+      <div className="accountContent__wrap">
+        <Button className="accountContent__button">View Product</Button>
+      </div>
+    </div>
+  );
+};
 
-export default OrderProduct
+export default OrderProduct;
