90b58880797ddacc66d4fbee2b12e40da1a656f0
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-21 14:01:05 +0700

    Из компонента orders вынесен в отдельный компонент orderproduct - он содержит блок одного заказа. Примечание: сейчас я в раздумьях по поводу структуры. С одной стороны, orderproduct почти идентичен тому, что есть в других компонентах по типу wallet и пр. У него та же сетка например. Но он по смыслу отличается, как будто. И стили у него в некоторых местах свои. В отдельный файл стили выносить не стала, хотя может и сделаю так, но там будет повтор общих стилей. Т.е. пока не знаю, как лучше разделить.

--- src/components/Pages/AccountPage/AccountContent/Orders.jsx
+++ src/components/Pages/AccountPage/AccountContent/Orders.jsx
@@ -1,6 +1,27 @@
 import React from "react";
-import Button from "../../../Common/Button/Button";
 import { openArrowIcon } from "../../../icons";
+import OrderProduct from "./OrderProduct";
+
+const orders = [
+  {
+    id: 1,
+    title: "Chic Shoes",
+    img: "https://cdn.shopify.com/s/files/1/0417/4987/2806/files/PF24_SANDALS_CREAM_FLOWER_RED_01_37e4370d-d3f7-4dbb-91c8-1897fe2ef83e.jpg?v=1713975584&width=768&crop=center",
+    date: "Aug 11, 2023",
+    status: "Recieved",
+    amount: 1,
+    price: 420,
+  },
+  {
+    id: 2,
+    title: "Elegant Swimsuit",
+    img: "https://cdn.shopify.com/s/files/1/0417/4987/2806/files/SS24_SWIMSUIT_01_BLACK_01.jpg?v=1706569090&width=768&crop=center",
+    date: "Aug 11, 2023",
+    status: "Recieved",
+    amount: 2,
+    price: 310,
+  },
+];
 
 const Orders = () => {
   return (
@@ -13,86 +34,9 @@ const Orders = () => {
         </div>
       </div>
 
-      <div className="accountContent__block">
-        <div className="accountContent__wrap accountContent__wrapImg">
-          <img
-            className="accountContent__img"
-            src="https://cdn.shopify.com/s/files/1/0417/4987/2806/files/PF24_SANDALS_CREAM_FLOWER_RED_01_37e4370d-d3f7-4dbb-91c8-1897fe2ef83e.jpg?v=1713975584&width=768&crop=center"
-            alt="Title of Product"
-          />
-        </div>
-        <div className="accountContent__wrap">
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Order Date:
-            </p>
-            <p className="accountContent__text">Aug 11, 2023</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Status
-            </p>
-            <p className="accountContent__status accountContent__statusRecieved">
-              Recieved
-            </p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Amount
-            </p>
-            <p className="accountContent__text">1 item</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Cost
-            </p>
-            <p className="accountContent__text">$420</p>
-          </div>
-        </div>
-        <div className="accountContent__wrap">
-          <Button className="accountContent__button">View Product</Button>
-        </div>
-      </div>
-      <div className="accountContent__block">
-        <div className="accountContent__wrap accountContent__wrapImg">
-          <img
-            className="accountContent__img"
-            src="https://cdn.shopify.com/s/files/1/0417/4987/2806/files/SS24_SWIMSUIT_01_BLACK_01.jpg?v=1706569090&width=768&crop=center"
-            alt="Title of Product"
-          />
-        </div>
-        <div className="accountContent__wrap">
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Order Date:
-            </p>
-            <p className="accountContent__text">July 11, 2024</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Status
-            </p>
-            <p className="accountContent__status accountContent__statusProgress">
-              In progress
-            </p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Amount
-            </p>
-            <p className="accountContent__text">2 items</p>
-          </div>
-          <div className="accountContent__info accountContent__infoStretch">
-            <p className="accountContent__subtitle accountContent__subtitleWithoutMargin">
-              Cost
-            </p>
-            <p className="accountContent__text">$820</p>
-          </div>
-        </div>
-        <div className="accountContent__wrap">
-          <Button className="accountContent__button">View Product</Button>
-        </div>
-      </div>
+      {orders.map((product) => {
+        return <OrderProduct key={product.id} info={product} />;
+      })}
     </div>
   );
 };
