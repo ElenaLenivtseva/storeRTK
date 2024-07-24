8de8359b83cd783729635170c21c1bcaa77e9dbb
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-07-24 23:38:56 +0700

    Исправлена ошибка с названием пропсов для компонента itemtotalcart. Созданы стили для totalart

--- src/components/Pages/CartPage/CartContent/Common/ItemTotalCart.jsx
+++ src/components/Pages/CartPage/CartContent/Common/ItemTotalCart.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ItemTotalCart = ({key, value}) => {
+const ItemTotalCart = ({info}) => {
   return (
     <div className="totalCart__item">
-      <p className="totalCart__itemText">{key}</p>
-      <p className="totalCart__itemText">{value}</p>
+      <p className="totalCart__itemText">{info.title}</p>
+      <p className="totalCart__itemText">{info.value}</p>
     </div>
   );
 };
--- src/components/Pages/CartPage/CartContent/Common/TotalCart.jsx
+++ src/components/Pages/CartPage/CartContent/Common/TotalCart.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import ItemTotalCart from "./ItemTotalCart";
+import './TotalCart.scss'
+import Button from "../../../../Common/Button/Button";
 
 const TotalCart = () => {
   return (
     <div className="totalCart">
       <div className="totalCart__header">
         <h4 className="subtitle totalCart__subtitle">Total Cart</h4>
-        <p className="totalCart__items">[2 items]</p>
+        <p className="totalCart__itemsAmount">[2 items]</p>
       </div>
       <div className="totalCart__body">
-        <ItemTotalCart key='Subtotal' value='$750'/>
-        <ItemTotalCart key='Shopping' value='Free'/>
-        <ItemTotalCart key='Total' value='$750'/>
+        <ItemTotalCart info={{title: 'Subtotal', value: '$750'}}/>
+        <ItemTotalCart info={{title: 'Shopping', value: 'Free'}}/>
+        <ItemTotalCart info={{title: 'Total', value: '$750'}}/>
+      </div>
+      <div className="totalCart__buttons">
+        <Button className='button_totalBlack totalCart__button'>Continue to Shipping</Button>
+        <Button className='button_withoutOutline totalCart__button'>Back to Store</Button>
       </div>
     </div>
   );
